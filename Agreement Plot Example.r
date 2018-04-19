c2c86640e851b7d9eaa2b611730fab110e5ef7fb
Derek R. Blanchette <drblanchette@gmail.com>
2018-04-19 15:55:09 -0500

    Create Agreement Plot Example.r

---
+++ Agreement Plot Example.r
@@ -0,0 +1,28 @@
+# Agreement Plot Example
+# Derek R. Blanchette
+
+#Load the measurements from the Formaldehyde data set
+data("Formaldehyde")
+
+#Data wrangling
+rating1 <- Formaldehyde[,1]
+rating2 <- Formaldehyde[,2] 
+ID <- seq(1:6)
+ratings <- as.data.frame(cbind(ID, rating1, rating2))
+
+#Create plot
+plot(ratings$rating1,
+     ratings$rating2,
+     main = "Agreement of Ratings",
+     xlim = c(min(ratings$rating1, ratings$rating2), max(ratings$rating1, ratings$rating2)),
+     ylim = c(min(ratings$rating1, ratings$rating2), max(ratings$rating1, ratings$rating2)),
+     xlab = "Rating 1",
+     ylab = "Rating 2",
+     pch = 19,
+     col = "blue")
+
+#Label samples
+text(ratings$rating1, ratings$rating2, labels = ratings$ID, pos = 2, cex = 0.7)
+
+#Add line of perfect agreement
+abline(0,1)
